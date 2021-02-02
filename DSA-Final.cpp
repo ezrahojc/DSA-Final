96724e8375ff955106a24fe4b495e49f0b3f29e6
Jincheng Ezra Ho <s10194982@connect.np.edu.sg>
2021-02-02 18:17:51 +0800

    added some validation for user input for option 1 and 2

--- DSA-Final.cpp
+++ DSA-Final.cpp
@@ -25,9 +25,15 @@ int main()
     {
         callMenu();
         int a;
-        cout << "\nPlease choose a number: ";
-        cin >> a;
+        cout << "\nPlease choose a choice: ";
 
+        while (!(cin >> a)) // validation to ensure only numbers
+        {
+            cout << "Invalid choice, please enter a valid choice: ";
+            cin.clear();
+            while (cin.get() != '\n'); // empty loop
+        }
+        
         // Name: Ezra Ho Jincheng.
         // Function: allows the user to input the details and parameters of the songs.
         // Input Parameters: addSID, addSong, addArtist, addGenre, addDuration.
@@ -36,8 +42,32 @@ int main()
         if (a == 1)
         {
             cout << "\n == Add Songs == \n";
-            cout << "Please enter the song ID: ";
-            cin >>  addSID;
+            cout << "Please enter the song ID (1 - 20): ";
+            cin >> addSID;
+            if (addSID >= 1 && addSID <= 20)
+            {
+                cout << "Valid song ID - within range.";
+            }
+            else if (addSID > 20)
+            {
+                cout << "Invalid song ID, please try again.\n";
+                continue;
+            }
+            else if (addSID < 0)
+            {
+                cout << "Invalid song ID, please try again.\n";
+                continue;
+            }
+            else
+            {
+                while (!(cin >> addSID)) // validation to ensure only numbers
+                {
+                    cout << "Invalid song ID, please try again.\n";
+                    cin.clear();
+                    break;
+                }
+                continue;
+            }
 
             cout << "\n\nPlease enter the song name: ";
             cin.ignore(); // clear one or more characters from input buffer.
@@ -51,6 +81,19 @@ int main()
 
             cout << "\n\nPlease enter the song duration: ";
             cin >> addDuration;
+            while (x == true)
+            {
+                if (cin >> addDuration)
+                {
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid song duration! Please input a numerical value." << endl;
+                    cin.clear();
+                    while (cin.get() != '\n'); // empty loop
+                }
+            }
 
             success = list1->add(addSID, addSong, addArtist, addGenre, addDuration); // add the song attributes into the list.
 
@@ -74,7 +117,7 @@ int main()
             list1->printList();
         }
 
-        // Name: Ezra Ho Jincheng
+        // Name: Ezra Ho Jincheng.
         // Function: allows the user to remove the song just by using the song ID keyed in by the user.
         // Input Parameters: addSID.
         // Output Parameters: a cout showing indicating that the song has been deleted / has not been deleted since the song does not exist.
@@ -111,25 +154,20 @@ int main()
         else if (a == 0)
         {
             cout << "Exiting... \n\n";
-            exit;
+            break;
         }
-
-        else
-        {
-            cout << "Incorrect input. Please try again.\n";
-        }
-
     }
 }
 
-// Name: Ezra Ho Jincheng
+// Name: Ezra Ho Jincheng.
 // Function: callMenu() function is used to print the menu at the start and a choice has been fully performed by the user.
 // Input Parameters: none
 // Output Parameters: none
 void callMenu()
 {
-    cout << "\nChoice\tAction\n";
-    cout << "------\t------\n";
+    cout << "\n======\t======\n";
+    cout << "Choice\tAction\n";
+    cout << "======\t======\n";
     cout << "1\tAdd song\n";
     cout << "2\tDisplay songs\n";
     cout << "3\tRemove songs\n";
